1c7616f5492f4c9ecd9553d3e8bf42e1b214a024
jukij3 <125390666+jukij3@users.noreply.github.com>
2023-02-15 20:54:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# rem = 'q'
+
 def read_from_file(filename):
     file = open(filename, 'r')
     names = tuple(file.readline()[:-1].split(','))
@@ -12,13 +14,13 @@ def read_from_file(filename):
         print(tup_str)
     return names, data
 
-
 def write_to_file(names, data):
     file = open('d2.csv', 'w')
     file.write(','.join(names)+'\n')
     for line in data:
         line = [str(i) for i in line]
-        file.write(','.join(line)+'\n')
+        file.write(','.join(line) + '\n')
+
     return file
 
 
@@ -27,10 +29,27 @@ def new_data(file_name):
     file = open(file_name, 'a')
     i = 0
     while i <= 5:
-        new = input()
-        new_data.append(new)
-        i += 1
+
+         new = input()
+         new_data.append(new)
+         i += 1
     file.write(','.join(new_data) + '\n')
+
+
+def count_lines(filename, chunk_size=1<<13):
+    with open(filename) as file:
+        return sum(chunk.count('\n')
+                   for chunk in iter(lambda: file.read(chunk_size), ''))
+
+def rem_data(file_name):
+    with open('data.csv', 'r') as file:
+        lines = file.readlines()
+        # lines = lines[:-1]
+        # print(lines)
+        lines.pop(int(rem))
+        # print(lines)
+    with open('data.csv', 'w') as file:
+        file.writelines(lines)
 
 
 names, data = read_from_file('data.csv')
@@ -38,15 +57,15 @@ write_to_file(names, data)
 print("Ввести данные?")
 print("1) Yes")
 print("2) No")
-print("3) Удалить последнюю строку")
+print("3) Удалить строку")
 answer = input()
 if answer == '1':
     new_data('data.csv')
+
 elif answer == '3':
-    with open('data.csv', 'r') as file:
-        lines = file.readlines()
-        lines = lines[:-1]
-    with open('data.csv', 'w') as file:
-        file.writelines(lines)
+    print("Какую строку удалить")
+    rem = input()
+    rem_data('data.csv')
+
 else:
     exit()
